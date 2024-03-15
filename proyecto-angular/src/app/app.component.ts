2647aefc0ebf7b7fdc6703f7f864d5fb23589f93
marcosgallardi <gallardimarcos98@gmail.com>
2024-03-15 14:20:26 -0300

    avanznado en el curso

--- proyecto-angular/src/app/app.component.ts
+++ proyecto-angular/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './components/user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,UserComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title:string = 'proyecto-angular';
+  title: string = 'proyecto-angular';
 }
